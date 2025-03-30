95a3d1b1f43f8ff8f41f2e66d4a03b8379e70a7e
amitdervadiya <amitdervadiya@gmail.com>
2025-03-30 11:06:58 +0530

    update

--- Employee Managment System/frontend/vite.config.js
+++ Employee Managment System/frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js'],
+      'application/json': ['json'],
+      'text/javascript': ['mjs']
+    }
+  }
 })
